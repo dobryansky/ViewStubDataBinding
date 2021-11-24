ca6a3828b482593e592a87f40fff8591686dd1db
Artem Dobryansky <artemd@wizardsdev.com>
2021-11-24 17:04:37 +0300

    States

--- app/src/main/java/viewstub/databinding/ViewModel.kt
+++ app/src/main/java/viewstub/databinding/ViewModel.kt
@@ -6,31 +6,9 @@ import android.view.View
 import androidx.lifecycle.MutableLiveData
 
 class ViewModel {
-    val state = MutableLiveData<State>().apply { value = State.Error }
-    val retryClickListener = View.OnClickListener { loadData() }
-    private var counter: Int = 0
-
-    init {
-        loadData()
-    }
-
-    fun loadData() {
-        state.value = State.Loading
-        Handler(Looper.getMainLooper())
-            .postDelayed(
-                {
-                    when (counter) {
-                        0 -> state.value = State.Error
-                        1 -> state.value = State.Empty
-                        else -> state.value = State.Done
-                    }
-                    counter++
-                }, 1000
-            )
-    }
+    val state = MutableLiveData<State>().apply { value = State.Empty }
 
     enum class State {
-         Loading,
          Empty ,
          Done ,
          Error
